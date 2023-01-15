95021d1d194e36ee01adca346f2b4f40364283a7
golfingtrex <77211832+golfingtrex@users.noreply.github.com>
2023-01-14 21:35:57 -0500

    Code cleanup and documentation. Final product.

--- GuessTheNumber/MainWindow.xaml.cs
+++ GuessTheNumber/MainWindow.xaml.cs
@@ -14,102 +14,47 @@ namespace GuessTheNumber
         public MainWindow()
         {
             InitializeComponent();
-            GenerateWinningNumber("Novice");
+            GenerateWinningNumber("Novice"); // Starting difficulty of "Novice".
             Loaded += Window_Loaded;
         }
 
+        /// <summary>
+        /// Generates a random number between 0 and a positive value based on selected difficulty called winningNumber
+        /// and sets the Text property of the WinningNumberTextBlock to winningNumber.
+        /// </summary>
+        /// <param name="difficulty">The difficulty setting of the current game.</param>
         private void GenerateWinningNumber(string difficulty)
         {
             Random random = new();
             int lowerBounds = 0;
-            int upperBounds;
-            switch (difficulty)
+            int upperBounds = difficulty switch
             {
-                case "Novice":
-                    upperBounds = 3;
-                    break;
-                case "Average":
-                    upperBounds = 10;
-                    break;
-                case "Experienced":
-                    upperBounds = 25;
-                    break;
-                case "Skilled":
-                    upperBounds = 50;
-                    break;
-                case "Adept":
-                    upperBounds = 75;
-                    break;
-                case "Masterful":
-                    upperBounds = 100;
-                    break;
-                case "Inhuman":
-                    upperBounds = 150;
-                    break;
-                case "Godlike":
-                    upperBounds = 250;
-                    break;
-                default:
-                    upperBounds = 3;
-                    break;
-            }
-
+                "Novice" => 3,
+                "Average" => 10,
+                "Experienced" => 25,
+                "Skilled" => 50,
+                "Adept" => 75,
+                "Masterful" => 100,
+                "Inhuman" => 150,
+                "Godlike" => 250,
+                _ => 3,
+            };
             int winningNumber = random.Next(lowerBounds, upperBounds + 1);
+
             WinningNumberTextBlock.Text = winningNumber.ToString();
         }
 
-        private void KeypadButton_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = sender as Button;
-            string buttonContent = button.Content as string;
-
-            if (buttonContent != null && GuessedNumberTextBlock.Text.Length < 3)
-            {
-                GuessedNumberTextBlock.Text += buttonContent;
-            }
-        }
-
-        private void CheckMarkButton_Click(object sender, RoutedEventArgs e)
-        {
-            SubmitGuess();
-        }
-
-        private void DifficultyRadioButton_Click(object sender, RoutedEventArgs e)
-        {
-            RadioButton radioButton = sender as RadioButton;
-            RestartGame(radioButton.Name);
-        }
-
-        private void RestartGame(string difficulty)
-        {
-            WinningNumberTextBlock.Background = new SolidColorBrush(Colors.Black);
-            GenerateWinningNumber(difficulty);
-            GuessedNumberTextBlock.Text = null;
-            GuessedNumberTextBlock.Background = new SolidColorBrush(Colors.White);
-        }
-
-        private void SubmitGuess()
-        {
-            if (!(String.IsNullOrEmpty(GuessedNumberTextBlock.Text)))
-            {
-                WinningNumberTextBlock.Background = new SolidColorBrush(Colors.White);
-                if (GuessedNumberTextBlock.Text == WinningNumberTextBlock.Text)
-                {
-                    GuessedNumberTextBlock.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#A3BE8C");
-                }
-                else
-                {
-                    GuessedNumberTextBlock.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#BF616A");
-                }
-            }
-        }
-
+        /// <summary>
+        /// Sets the Click event for all Buttons and RadioButtons in the window.
+        /// </summary>
+        /// <param name="sender">The MainWindow.</param>
+        /// <param name="e">TODO - idk much about this.</param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             IEnumerable<Button> buttons = GetChildButtons(KeypadGrid);
             foreach (Button button in buttons)
             {
-                if (button != null && button.Name is not "CheckMarkButton")
+                if (button != null && button.Name != "CheckMarkButton")
                 {
                     button.Click += KeypadButton_Click;
                 }
@@ -129,48 +74,115 @@ namespace GuessTheNumber
             }
         }
 
-        private void Play_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private IEnumerable<Button> GetChildButtons(DependencyObject parent)
+        /// <summary>
+        /// Collects all Buttons in the window.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <returns>An IEnumerable collection of Buttons.</returns>
+        private static IEnumerable<Button> GetChildButtons(DependencyObject parent)
         {
             var children = LogicalTreeHelper.GetChildren(parent);
             var buttons = new List<Button>();
             foreach (var child in children)
             {
-                if (child is Button)
+                if (child is Button button)
                 {
-                    buttons.Add((Button)child);
+                    buttons.Add(button);
                 }
-                else if (child is DependencyObject)
+                else if (child is DependencyObject dependencyObject)
                 {
-                    buttons.AddRange(GetChildButtons((DependencyObject)child));
+                    buttons.AddRange(GetChildButtons(dependencyObject));
                 }
             }
             return buttons;
         }
 
-        private IEnumerable<RadioButton> GetChildRadioButtons(DependencyObject parent)
+        /// <summary>
+        /// Collects all RadioButtons in the window.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <returns>An IEnumerable collection of RadioButtons.</returns>
+        private static IEnumerable<RadioButton> GetChildRadioButtons(DependencyObject parent)
         {
             var children = LogicalTreeHelper.GetChildren(parent);
             var radioButtons = new List<RadioButton>();
             foreach (var child in children)
             {
-                if (child is RadioButton)
+                if (child is RadioButton button)
                 {
-                    radioButtons.Add((RadioButton)child);
+                    radioButtons.Add(button);
                 }
-                else if (child is DependencyObject)
+                else if (child is DependencyObject dependencyObject)
                 {
-                    radioButtons.AddRange(GetChildRadioButtons((DependencyObject)child));
+                    radioButtons.AddRange(GetChildRadioButtons(dependencyObject));
                 }
             }
 
             return radioButtons;
         }
 
+        /// <summary>
+        /// Appends the content of the keypad button that is clicked to the GuessedNumberTextBlock Text property,
+        /// but only if the current length has three or less digits.
+        /// </summary>
+        /// <param name="sender">The keypad Button that was clicked.</param>
+        /// <param name="e">TODO - idk much about this.</param>
+        private void KeypadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button? button = sender as Button;
 
+            if (button?.Content is string buttonContent && GuessedNumberTextBlock.Text.Length < 3)
+            {
+                GuessedNumberTextBlock.Text += buttonContent;
+            }
+        }
+
+        /// <summary>
+        /// Reveals the winning number and sets the background of the guessed number to green if it's a match, sets it to
+        /// red if not.
+        /// </summary>
+        /// <param name="sender">The Button that was clicked. i.e. the green check mark Button.</param>
+        /// <param name="e">TODO - idk much about this.</param>
+        private void CheckMarkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(String.IsNullOrEmpty(GuessedNumberTextBlock.Text)))
+            {
+                WinningNumberTextBlock.Background = new SolidColorBrush(Colors.White);
+                if (GuessedNumberTextBlock.Text == WinningNumberTextBlock.Text)
+                {
+                    GuessedNumberTextBlock.Background = new BrushConverter().ConvertFromString("#A3BE8C") as SolidColorBrush;
+                }
+                else
+                {
+                    GuessedNumberTextBlock.Background = new BrushConverter().ConvertFromString("#BF616A") as SolidColorBrush;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calls RestartGame with the Name of the RadioButton as the difficulty.
+        /// </summary>
+        /// <param name="sender">The RadioButton that was checked.</param>
+        /// <param name="e">TODO - idk much about this.</param>
+        private void DifficultyRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                RestartGame(radioButton.Name);
+            }
+        }
+
+        /// <summary>
+        /// Sets the Background property of the WinningNumberTextBlock to Black, generates a new winning number,
+        /// clears out the user's guessed number and resets the Background of the GuessedNumberTextBlock to White.
+        /// </summary>
+        /// <param name="difficulty">The difficulty setting of the new game. Used in generating the winning number.</param>
+        private void RestartGame(string difficulty)
+        {
+            WinningNumberTextBlock.Background = new SolidColorBrush(Colors.Black);
+            GenerateWinningNumber(difficulty);
+            GuessedNumberTextBlock.Text = null;
+            GuessedNumberTextBlock.Background = new SolidColorBrush(Colors.White);
+        }
     }
 }
